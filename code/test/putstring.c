baa7c6009eb09f0aea91ec4e21e2211d91c7c190
clercmargo <clercmargo@gmail.com>
2012-01-04 09:27:55 +0000

    nouvelle version clean et avec putstring

---
+++ code/test/putstring.c
@@ -0,0 +1,9 @@
+#include "syscall.h"
+
+int main(){
+	PutString("bienvenue en tp nachos");
+	PutChar('\n');
+	PutString("voici une super phrase qui est superbement trop longue et va donc se faire couper par l'appel a copy string from machine hhihihihihihih");
+	PutChar('\n');
+	return 0;
+}
