c365bddd75f2c1feaed3788e4ad37129c1dc7e06
tofan.alexandre <tofan.alexandre@6a8a138e-4e4a-1d1e-9914-2e89ee241388>
2012-01-16 13:35:32 +0000

    Frame Provider Class

---
+++ code/userprog/frameprovider.cc
@@ -0,0 +1,63 @@
+#include "frameprovider.h"
+#include "system.h"
+
+FrameProvider::FrameProvider(){
+
+	numFrames=NumPhysPages;
+	map = new unsigned int[numFrames];
+    for (int i = 0; i < numFrames; i++)
+	ReleaseFrame (i);
+
+}
+
+FrameProvider::~FrameProvider(){
+
+	delete [] map;
+
+}
+
+bool FrameProvider::TestFrame (int which)
+{
+    ASSERT (which >= 0 && which < numFrames);
+
+    if (map[which]==1)
+	return TRUE;
+    else
+	return FALSE;
+}
+
+void FrameProvider::MarkFrame (int which)
+{
+    ASSERT (which >= 0 && which < numFrames);
+    map[which]=1;
+}
+
+
+int FrameProvider::GetEmptyFrame(){
+	    
+	for (int i = 0; i < numFrames; i++)
+	if (!TestFrame (i))
+	  {
+	      MarkFrame(i);
+	      return i;
+	  }
+    return -1;
+
+}
+
+void FrameProvider::ReleaseFrame(int which){
+
+	ASSERT (which >= 0 && which < numFrames);
+    map[which]=0;
+}
+
+int FrameProvider::NumAvailFrame(){
+
+    int count = 0;
+
+    for (int i = 0; i < numFrames; i++)
+	if (!TestFrame (i))
+	    count++;
+    return count;
+
+}
