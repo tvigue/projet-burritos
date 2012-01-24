c5ddc7579076cb2070cf2ff43fec0664e3403172
sophie.penot07 <sophie.penot07@6a8a138e-4e4a-1d1e-9914-2e89ee241388>
2012-01-24 09:17:44 +0000

    process done

--- code/test/step4_Fork.c
+++ code/test/step4_Fork.c
@@ -7,14 +7,13 @@ int main()
 	ForkExec("build/putstring");
 	ForkExec("build/putstring");
 	ForkExec("build/putstring");
-	ForkExec("build/putstring");
-	ForkExec("build/putstring");
-	ForkExec("build/putstring");
-	ForkExec("build/putstring");
-	ForkExec("build/putstring");
 	for(i=0;i<100000;i++){}
 	PutString("BBBBBBB\n");
-	//ForkExec("build/putstring");
+	ForkExec("build/putstring");
+	ForkExec("build/putstring");
+	ForkExec("build/putstring");
+	ForkExec("build/putstring");
+	ForkExec("build/putstring");
 	Exit(0);
 	
 }
